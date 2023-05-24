e4ab8984520f8754cfa1fb3bb6b7b9f010d2a270
KBradshaw64 <130187631+KBradshaw64@users.noreply.github.com>
2023-05-23 21:24:42 -0700

    added ability to change the location searched for in the fetch function

--- index.js
+++ index.js
@@ -2,16 +2,19 @@ var searchBtn = document.getElementById('search-btn');
 
 
 function getApi() {
-    var requestUrl = 'https://api.openweathermap.org';
+    var cityName = document.getElementById('search-input')
+    var requestUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=' + cityName.textContent + '&limit=3&units=imperial&appid=61b1a371ad3841eac980339ee6ae116f';
 
     fetch(requestUrl)
-        console.log(requestUrl)
         .then(function(response){
-            console.log(response)
             return response.json();
         })
         .then(function(data){
             console.log(data);
+            var cityCast = document.getElementById('current-forecast')
+            var fiveDay = document.getElementById('5-day')
+            cityCast.textContent = data.list.main.temp
+
         });
 }
 
